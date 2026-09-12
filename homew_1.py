bdb0c5e125e1b5195ce9219b1b88da0473b7e50f
Yevheniia <sopli15011970@gmail.com>
2026-09-12 17:59:31 +0200

    commit3

--- homew_1.py
+++ homew_1.py
@@ -2,14 +2,14 @@ from datetime import datetime
 
 def get_days_from_today(date: str) ->int:
     try:
-        targer_day = datetime.strptime(date,  "%Y.%m.%d").date()
+        targer_day = datetime.strptime(date,  "%Y-%m-%d").date()
         today_day = datetime.today().date()
         difference = today_day - targer_day
         
         return difference.days
     except ValueError:
-        print(ValueError ("Wrong date format. Please enter the date in the correct format PPPP.MM.DD, for example(2020.10.09)"))
+        print("Wrong date format. Please enter the date in the correct format PPPP.MM.DD, for example(2020.10.09)")
         
 
-print(get_days_from_today('2020.10.09'))
+print(get_days_from_today('2020-10-09'))
     
--- homew_2.py
+++ homew_2.py
@@ -2,7 +2,7 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
-        return set()
+        return []
     
     unique_numbers = set()
     
